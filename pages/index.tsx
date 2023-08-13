ca283481c2cb1c896a24f362b6f97f571d7230ab
vofes <bielovlad@gmail.com>
2023-08-13 19:30:57 +0200

    fix images 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import { prefix } from "@/pages/githubPrefix"
+import { prefix } from "@/githubPrefix"
 
 const inter = Inter({ subsets: ['latin'] })
 
