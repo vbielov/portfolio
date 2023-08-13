fabd10db320905d5a04ae382abdcb9b749d75c6a
vofes <bielovlad@gmail.com>
2023-08-13 19:35:11 +0200

    fix images 3

--- githubPrefix.js
+++ githubPrefix.js
@@ -1,3 +1,9 @@
-const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
+var prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+if (isGithubActions) {
+    prefix = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+}
 
 export { prefix };
