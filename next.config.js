ad543c565e8a90e89bbe2da3ad2d81003245e40b
vbielov <v.bielov@hm.edu>
2025-04-05 18:11:26 +0200

    process.env

--- next.config.js
+++ next.config.js
@@ -1,27 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = ''
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = '/portfolio';
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 const nextConfig = {
   trailingSlash: true,
   reactStrictMode: true,
   images: {
     unoptimized: true
   },
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   output: 'export',
+  assetPrefix: process.env.ASSET_PREFIX,
+  basePath: process.env.BASE_PATH,
 }
 
 module.exports = nextConfig
